42532afa1c667d4f1a57f79576e47a65e5a6ffdf
harsh2318 <harshagowda2318@gmail.com>
2025-05-14 11:57:49 +0530

    removed app.log file

--- backend/app.py
+++ backend/app.py
@@ -33,22 +33,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Create handlers
-file_handler = RotatingFileHandler(
-    os.path.join(log_dir, 'app.log'),
-    maxBytes=1024 * 1024,  # 1MB
-    backupCount=5,
-    encoding='utf-8'
-)
 stream_handler = logging.StreamHandler()
 
 # Create formatters and add it to handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
 
 # Add handlers to the logger
-logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 # load_dotenv('/etc/bharatai.env')
